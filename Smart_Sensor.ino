031a75c218aa6b4d383a62e8820bd3a43d0ecafa
sverlon <75929507+sverlon@users.noreply.github.com>
2020-12-13 04:48:25 -0500

    Add files via upload

---
+++ Smart_Sensor.ino
@@ -0,0 +1,71 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include "DHT.h"
+#include <ArduinoHttpClient.h>
+
+char ssid[] = "ATTAB6jbtA";
+char pass[] = "fv3fw6t=k3r6";
+
+char serverAddress[] = "192.168.1.68";  // server address
+int port = 8080;
+
+WiFiClient wifi;
+WebSocketClient client = WebSocketClient(wifi, serverAddress, port);
+int status = WL_IDLE_STATUS;
+   
+void setup() {
+    
+//  sensor.begin(9600);
+  Serial.begin(9600);
+  
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  client.begin();
+}
+
+void loop() {
+  
+  int sound = digitalRead(7);
+  
+// DHT sensor(2, DHT11);
+//float temperature = sensor.readTemperature();
+//float humidity = sensor.readHumidity();
+
+  
+  if ( (millis() % 2000) == 0 ) {
+
+    client.beginMessage(TYPE_TEXT); 
+    client.print(millis());
+    client.print(",");
+    client.println(sound);
+    client.endMessage();
+    
+    delay(100);
+  }
+
+  if (sound == 1) {
+
+    client.beginMessage(TYPE_TEXT);   
+    client.print(millis());
+    client.print(",");
+    client.println(sound);
+    client.endMessage();
+    
+    delay(500);
+  }
+}
